8dabd335865c27d6071e854e02febf2d9c4302d1
BSquancher <239266402+BSquancher@users.noreply.github.com>
2026-03-24 17:02:05 -0500

    Human Selection Made it so the USER can input a selection

--- rpsJS.js
+++ rpsJS.js
@@ -1,5 +1,36 @@
 
 let computerHand = ""; //computerHand is a STRING that will be the computer's selection
+let humanHand = ""; //humnaHand is a STRING that will be the USER's selection
+
+const roundButton = document.querySelector("#roundButton"); //play a single round
+const matchButton = document.querySelector("#matchButton"); // play an entire match (5 rounds)
+
+const log = document.querySelector("#scoreBoard") //scoreBoard Log
+
+//On click of button it will ask you to type your answer
+
+roundButton.addEventListener("click", () =>{
+  let sign = prompt("Choose ROCK, PAPER OR SCISSORS!");
+
+    if (sign === null){
+      log.innerText = "Please, choose your character!";
+    } else if (sign.toLowerCase() === ""){
+      log.innerText = "Please, make a selection";
+    } else if (sign.toUpperCase() === "ROCK"){
+      log.innerText = "ROCK!";
+    } else if (sign.toUpperCase() === "PAPER"){
+      log.innerText = "PAPER!";
+    } else if (sign.toUpperCase() === "SCISSORS"){
+      log.innerText = "SCISSORS!";
+    } else {
+      log.innerText = `${sign} is NOT a valid selection!`;
+    }
+
+});
+
+
+
+
 
 //RAND function for helping computer select move
 function getRandomInt(){
@@ -26,6 +57,12 @@ if (selectionNumber == 0){
   return computerHand;
 }
 
+function getHumanChoice(){
+
+  prompt("Please Type ROCK PAPER OR SCISSORS!")
+}
+
 computerHand = getComputerChoice();
 
 console.log(computerHand);
+console.log(humanHand);
